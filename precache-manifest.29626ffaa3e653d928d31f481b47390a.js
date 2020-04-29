ad1218e64898f03fa4708740289cc0281516de7d
Meenakshi121 <meenakshikokkiligadda@gmail.com>
2020-04-29 21:42:19 +0530

    Updates

---
+++ precache-manifest.29626ffaa3e653d928d31f481b47390a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbacec00df98ef102d3a7c1c3f9e11c6",
+    "url": "/react-profile/index.html"
+  },
+  {
+    "revision": "e0a4179af0bee6f0c017",
+    "url": "/react-profile/static/css/main.8f317531.chunk.css"
+  },
+  {
+    "revision": "0f5b94cfe4a2492a9984",
+    "url": "/react-profile/static/js/2.c033a476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-profile/static/js/2.c033a476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a4179af0bee6f0c017",
+    "url": "/react-profile/static/js/main.5d9cffa1.chunk.js"
+  },
+  {
+    "revision": "9ed281195f38b6cb54f7",
+    "url": "/react-profile/static/js/runtime-main.94118734.js"
+  }
+]);
